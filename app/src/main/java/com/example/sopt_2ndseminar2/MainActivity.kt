f6079ee0f7a2bc8e7168d8e1b9a75a3f821a537f
Jungmin <ljm9748@naver.com>
2020-04-28 18:05:11 +0900

    5th

--- app/src/main/java/com/example/sopt_2ndseminar2/MainActivity.kt
+++ app/src/main/java/com/example/sopt_2ndseminar2/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.sopt_2ndseminar2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
+
+typealias onPageChageListener = Any
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_viewPager.adapter=MainPagerAdapter(supportFragmentManager)
+        main_viewPager.offscreenPageLimit = 2
+        main_viewPager.addOnPageChangeListener( object :ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+
+        bottomNavigationView.setOnNavigationItemReselectedListener{
+            when(it.itemId){
+                R.id.menu_home -> main_viewPager.currentItem = 0
+                R.id.menu_book -> main_viewPager.currentItem = 1
+                R.id.menu_person -> main_viewPager.currentItem = 2
+
+            }
+            true
+
+        }
     }
 }
